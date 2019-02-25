26c305e2a8a1e0523a1aff3d0c1c6ab207bb14ad
huangHaoQi <2609352912@qq.com>
2019-02-25 16:32:41 +0800

    111

--- src/main/java/com/warehouse/controller/FinanceController.java
+++ src/main/java/com/warehouse/controller/FinanceController.java
@@ -12,19 +12,19 @@ public class FinanceController {
 		System.out.println("账户信息");
 		return "page/finance/accountInfo";
 	}
-	
+
 	@RequestMapping("/paymentDoc")
 	public String paymentDoc() {
 		System.out.println("付款单");
 		return "page/finance/paymentDoc";
 	}
-	
+
 	@RequestMapping("/receiptsDoc")
 	public String receiptsDoc() {
 		System.out.println("收款单");
 		return "page/finance/receiptsDoc";
 	}
-	
+
 	@RequestMapping("/profitAnalysis")
 	public String profitAnalysis() {
 		System.out.println("利润分析");
--- src/main/java/com/warehouse/mapper/COMAccountMapper.java
+++ src/main/java/com/warehouse/mapper/COMAccountMapper.java
@@ -1,10 +1,15 @@
 package com.warehouse.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
 import com.warehouse.pojo.COMAccount;
 
+@Mapper
 public interface COMAccountMapper {
     int deleteByPrimaryKey(Integer id);
-
+ 
     int insert(COMAccount record);
 
     int insertSelective(COMAccount record);
@@ -14,4 +19,11 @@ public interface COMAccountMapper {
     int updateByPrimaryKeySelective(COMAccount record);
 
     int updateByPrimaryKey(COMAccount record);
+
+    /**
+     * 查询公司账户信息
+     * @return
+     */
+    List<COMAccount> selectCOMAccountInfo();
+    
 }
